2fc0c6639db8428df02c5e5e0948c3cde1265e1e
go2665 <go2665@outlook.com>
2026-08-19 11:02:58 +0900

    인벤토리 위젯 클래스 구현중

--- Source/Unreal10th_CPP/Private/Widget/Inventory/InventoryWidget.cpp
+++ Source/Unreal10th_CPP/Private/Widget/Inventory/InventoryWidget.cpp
@@ -2,4 +2,90 @@
 
 
 #include "Widget/Inventory/InventoryWidget.h"
+#include "Widget/Inventory/MoneyPanelWidget.h"
+#include "Widget/Inventory/InventorySlotWidget.h"
+#include "Components/Button.h"
+#include "Components/UniformGridPanel.h"
+#include "Component/InventoryComponent.h"
 
+
+void UInventoryWidget::InitializeInventoryWidget(UInventoryComponent* InInven)
+{
+	ClearInventoryWidget();
+	TargetInventory = InInven;
+
+	if (!TargetInventory.IsValid())
+	{
+		UE_LOG(LogTemp, Log, TEXT("TargetInventory가 Valid하지 않습니다."));
+		return;
+	}
+
+	if (SlotGridPanel)
+	{
+		int32 ChildCount = SlotGridPanel->GetChildrenCount();
+		int32 InvenSize = TargetInventory->GetSize();
+		int32 Size = FMath::Min(ChildCount, InvenSize);
+
+		SlotWidgets.Empty(Size);
+		for (int i = 0; i < Size; i++)
+		{
+			if (UInventorySlotWidget* SlotWidget = Cast<UInventorySlotWidget>(SlotGridPanel->GetChildAt(i)))
+			{
+				SlotWidget->InitializeSlot(TargetInventory.Get(), i);
+				SlotWidgets.Add(SlotWidget);
+			}
+		}
+	}
+
+}
+
+void UInventoryWidget::ClearInventoryWidget()
+{
+	TargetInventory = nullptr;
+}
+
+void UInventoryWidget::OpenInventoryWidget()
+{
+}
+
+void UInventoryWidget::CloseInventoryWidget()
+{
+}
+
+void UInventoryWidget::RefreshInventoryWidget() const
+{
+	if (!TargetInventory.IsValid()) return;
+
+	RefreshMoneyPanel(TargetInventory->GetMoney());
+
+	for (const UInventorySlotWidget* SlotWidget : SlotWidgets)
+	{
+		if (SlotWidget)
+		{
+			SlotWidget->RefreshSlot();
+		}
+	}
+}
+
+void UInventoryWidget::RefreshSlotWidget(int32 InSlotIndex) const
+{
+}
+
+void UInventoryWidget::RefreshMoneyPanel(int32 InCurrentMoney) const
+{
+}
+
+void UInventoryWidget::NativeConstruct()
+{
+	Super::NativeConstruct();
+	if (CloseButton)
+	{
+		CloseButton->OnClicked.AddDynamic(this, &UInventoryWidget::OnClickedCloseButton);
+	}
+}
+
+void UInventoryWidget::OnClickedCloseButton()
+{
+	UE_LOG(LogTemp, Log, TEXT("닫기 버튼 눌려짐"));
+	CloseInventoryWidget();
+}
--- Source/Unreal10th_CPP/Private/Widget/Inventory/MoneyPanelWidget.cpp
+++ Source/Unreal10th_CPP/Private/Widget/Inventory/MoneyPanelWidget.cpp
@@ -2,4 +2,9 @@
 
 
 #include "Widget/Inventory/MoneyPanelWidget.h"
+#include "Components/TextBlock.h"
 
+void UMoneyPanelWidget::SetMoney(int32 InMoney)
+{
+	MoneyText->SetText(FText::AsNumber(InMoney));
+}
